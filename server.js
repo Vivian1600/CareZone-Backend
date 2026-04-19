5ef73f376b042cb26ae5227af4a0599ac0743f79
Vivian1600 <mutioomondi@gmail.com>
2026-04-19 23:16:40 +0300

    modified

--- server.js
+++ server.js
@@ -149,9 +149,10 @@ app.use(errorHandler);
 // Start Server
 // =====================================================
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log('=================================');
     console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
     console.log(`📝 Environment: ${process.env.NODE_ENV || 'development'}`);
     console.log('=================================');
 });
